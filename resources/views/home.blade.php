a4ce949b9cd7d89fcc081bb3985a5cd372b3bc07
agnesvarga1 <agikolyok@gmail.com>
2024-03-15 16:06:42 +0100

    set up + db conn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/js/app.js')
-    <title>@yield("title")</title>
-</head>
-<body>
- <h1 class="text-center text-success">My Template</h1>
-</body>
-</html>
+@extends('layouts.app')
+@section("title","Train Info | Home")
+@section("main")
+<h1 class="text-center text-primary">Train Infos</h1>
+
+@endsection
